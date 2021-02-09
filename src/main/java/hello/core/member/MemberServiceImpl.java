73fe3f986277dff016c3a712b3887f9c7b2ab0ce
Jungmin-Seo <soato1405@naver.com>
2021-02-09 19:28:17 +0900

    회원 도메인 실행과 테스트
    오직 자바만을 이용한 테스트 (MemberServiceApp)
    member 객체 하라늘 생성하고 join 시킨후
    memberService 객체에서 member 아이디로 join 시킨 member 를 찾는다.
    join 시킨 맴버와 id로 찾은 맴버가 같은 이름이면 테스트 통과
    
    같은 방법을 junit 를 이용해서 구현한 것이 MemberServiceTest
    given: memberA 이름을 가진 맴버 생성(Id: 1L)
    when: memberA 회원가입, ID(1L)로 findMember
    then: 처음 생성한 맴버와 findMember 의 맴버가 같은 객체인지 판별
    
    여기까지 오면 MemberRepository 가 인터페이스인 MemberRepository 와
    구현체인 MemoryMemberRepository 모두 의존하고 있다.
    즉 의존관계가 인터페이스 뿐만 아니라 구현까지 모두 의존하는 문제점이 있다.
    실제 다른 저장소로 변경할 때 OCP 원칙을 지킬 수 없다.

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 구현체를 의존해버림(DIP 위반)
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
