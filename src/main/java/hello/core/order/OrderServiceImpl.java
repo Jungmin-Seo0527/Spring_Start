6a75df49632338b34e5c969e2ed9e591ff7c6b68
Jungmin-Seo0527 <soato1405@naver.com>
2021-02-20 18:50:58 +0900

    롬복과 최신 트랜드
    롬복이라는 라이브러리를 설치하여 적용하였다.
    코드를 짜면 getter, setter, constructor 등등을 의무적으로 짜는 경우가 많다. 그리고 생성자 주입 방식을 위해서라도 생성자는 필수로 만들어 주어야 한다.
    
    하지만 롬북은 애노테이션 추가만으로 자동으로 모든 작업들을 자동으로 해준다.
    OrderServiceImpl 클래스를 보면 기존의 생성자 주입을 위해서 생성자를 만들어 주고 @Autowired 를 해준 모습을 볼수 있다.
    하지만 롬복을 적용하면 @RequiredArgusConstructor 애노테이션 한줄만 추가하여 생성자 코드를 생략할 수 있다. 이는 실제로는 모두 구현이 되어 있는 상태이다.
    
    롬복을 이용하면 생성자 코드를 따로 짜야되는 번거로움을 피할수 있으며 이후에 의존성이 추가 될때, 필드만 추가해주면 된다. 그리고 final 도 유지할수 있다.
    현업에서는 필수적으로 이용하는 라이브러리라고 봐도 무방하다.
    -------------------------------------------------------------------------------------------------
    막상 개발을 해보면, 대부분으 다 불변이고, 그래서 다음과 같이 생성자에 final 키워드를 사용하게 된다.
    
    그런데 생성자도 만들어야 하고, 주입 받는 값을 대입하는 코드도 만들어야 하고...
    필드 주입처럼 좀 편리하게 사용하는 방법이 없을까?
    롬복으로 최적화를 할수 있다.
    
    build.gradle 에 롬복 라이브러리를 추가해주고 플러그인을 설치후에, 설정에서 Annotation Processors 를 활성화 시켜주어야 한다.
    
    롬복 라이브러리가 제공하는 @RequiredArgsConstructor 기능을 사용하면 final 이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다.
    롬복이 자바의 애노테이션 프로세서라는 기능을 이용해서 컴파일 시점에 생성자 코드를 자동으로 생성해준다. 실제 class 를 열어보면 생성자 코드가 추가되어 있다.
    
    정리)
    최근에는 생성자를 딱 1개 두고, @Autowired 를 생략하는 방법을 주로 사용한다. 여기에 Lombok 라이브러리의 @RequiredArgsConstructor 를 사용하면 기능은 다 제공하면서, 코드는 깔끔하게 사용할 수 있다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,41 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
-    // @Autowired // 필드 주입
     private final MemberRepository memberRepository;
-    // @Autowired
     private final DiscountPolicy discountPolicy;
-
-    @Autowired // 생성자 주입
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    // @Autowired // 수정자 주입(setter 주입)
-    // public void setMemberRepository(MemberRepository memberRepository) {
-    //     System.out.println("memberRepository = " + memberRepository);
-    //     this.memberRepository = memberRepository;
-    // }
-    //
-    // @Autowired
-    // public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-    //     System.out.println("discountPolicy = " + discountPolicy);
-    //     this.discountPolicy = discountPolicy;
-    // }
-    //
-    // @Autowired // 일반 메서드 주입
-    // public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    //     this.memberRepository = memberRepository;
-    //     this.discountPolicy = discountPolicy;
-    // }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
