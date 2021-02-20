d85e5ae8f8331eebb74ef4ce6b2fd2f74ac8c901
Jungmin-Seo0527 <soato1405@naver.com>
2021-02-20 14:52:02 +0900

    다양한 의존관계 주입 방법
    의존관계 주입 방법에는 4가지 방법이 있다.
    1. 생성자 주입
    생성자 주입은 지금까지 적용했던 의존성 주입 방법이다.
    생성자를 생성해서 Autowired 애노테이션을 추가해주면, 스피링 빈에 등록되어 있는 인스턴스들이 자동적으로 주입되는 방식이다.
    또한 생성자가 단 1개만 존재한다면 Autowired 애노테이션을 생략해도 된다.
    
    2. 수정자 주입
    setter 를 따로 만들어서 Autowired 애노테이션을 추가해주는 방식이다. 단 필드의 final 을 지워야지 set 이 가능하다. (당연히...)
    
    3. 필드 주입
    필드에 바로 @Autowired 를 추가해 주는 방식이다.
    코드가 간결해지고 간단하기 때문에 많은 개발자들을 유혹해온 방식이지만 좋은 방법은 아니다.
    외부에서 변경이 불가능해서 테스트 하기 힘들어 진다.
    
    4. 일반 메서드 주입
    일반 메서드에 @Autowired 를 추가해주는 방식인데 많이 사용되지는 않는 방식이라고 한다.
    
    --------------------------------------------------------------------------------------------------
    의존관계 주입은 크게 4가지 방법이 있다.
    생성자 주입
    수정자 주입(setter 주입)
    필드 주입
    일반 메서드 주입
    
    생성자 주입
    이름 그대로 생성자를 통해서 의존 관계를 주입 받는 방법이다.
    지금가지 진행했던 방법이 바로 생성자 주입이다.
    특징
    생성자 호출시점에 닥 1번만 호출되는 것이 보장된다.
    불변, 필수 의존관계에 사용 !!!
    중요) 생성자가 딱 1개만 있으면 @Autowired 를 생략해도 자동 주입이 된다. 물론 스프링 빈에만 해당한다.
    
    수정자 주입(Setter 주입)
    setter 라 불리는 필드의 값을 변경하는 수정자 메서드를 통해서 의존관계를 주입하는 방법이다.
    특징
    선택, 변경 가능성이 있는 의존관계에 사용 !!!
    자바빈 프로퍼티 규약의 수정자 메서드 방식을 사용하는 방법이다.
    참고) @Autowired 의 기본 동작은 주입할 대상이 없으면 오류가 발생한다. 주입할 대상이 없어도 동작하게 하려면 @Autowired(required = false) 로 지정하면 된다.
    
    참고) 자바빈 프로퍼티, 자바에서는 과거부터 필드의 값을 직접 변경하지 않고, setXxx, getXxx 라는 메서드를 통해서 값을 읽거나 수정하는 규칙을 만들었는데, 그것이 자바빈 프로퍼티 규약이다.
    
    필드 주입
    이름 그대로 필드에 바로 주입하는 방법이다.
    특징
    코드가 간결해서 많은 개발자들을 유혹하지만 외부에서 변경이 불가능해서 테스트 하기 힘들다는 치명적인 단점이 있다.
    DI 프레임워크가 없으면 아무것도 할 수 없다.
    사용하지 말자!!!(애플리케이션의 실제 코드와 관계 없는 테스트 코드, 스프링 설정을 목적으로 하는 @Configuration 같은 곳에서만 특별한 용도로 사용
    
    참고) 순수한 자바 테스트 코드에는 당연히 @Autowired 가 동작하지 않는다. @SpringBootTest 처럼 스프링 컨테이너를 테스트에 통합한 경우에만 가능하다.
    
    일반 메서드 주입
    일반 메서드를 통해서 주입 받을 수 있다.
    특징
    한번에 여러 필드를 주입 받을 수 있다.
    일반적으로 잘 사용하지 않는다.
    
    참고) 어쩌면 당연한 이야기이지만 의존관계 자동 주입은 스프링 컨테이너가 관리하는 스프링 빈이어야 동작한다. 스프링 빈이 아닌 Member 같은 클래스에서 @Autowired 코드를 적용해도 아무 기능도 동작하지 않는다.

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,23 +9,41 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    // @Autowired // 필드 주입
     private final MemberRepository memberRepository;
+    // @Autowired
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    @Autowired // 생성자 주입
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
+    // @Autowired // 수정자 주입(setter 주입)
+    // public void setMemberRepository(MemberRepository memberRepository) {
+    //     System.out.println("memberRepository = " + memberRepository);
+    //     this.memberRepository = memberRepository;
+    // }
+    //
+    // @Autowired
+    // public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+    //     System.out.println("discountPolicy = " + discountPolicy);
+    //     this.discountPolicy = discountPolicy;
+    // }
+    //
+    // @Autowired // 일반 메서드 주입
+    // public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    //     this.memberRepository = memberRepository;
+    //     this.discountPolicy = discountPolicy;
+    // }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
-
-        // discountPolicy -> 단일체계원칙을 잘 지킨 예시
-        // OrderServiceImpl 입장에서는 discountPolicy 의 내용을 알지 못하고 그 결과반 반환 받는다.
-        // 할인 정책이 바뀌게 되더라도 discountPolicy 의 내용만 변할뿐 OrderServiceImpl 은 변할 필요 없다.
         int discountPrice = discountPolicy.discount(member, itemPrice);
+
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
