6de823d31096a39100903767a7093acbba9df339
Jungmin-Seo0527 <soato1405@naver.com>
2021-03-15 20:43:42 +0900

    Update SingletonTest.java

--- src/test/java/hello/core/scope/SingletonTest.java
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -1,9 +1,8 @@
 package hello.core.scope;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import
+        org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
@@ -12,18 +11,16 @@ import javax.annotation.PreDestroy;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SingletonTest {
-
     @Test
-    void singletonBeanFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
-
+    public void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new
+                AnnotationConfigApplicationContext(SingletonBean.class);
         SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
         SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
         System.out.println("singletonBean1 = " + singletonBean1);
         System.out.println("singletonBean2 = " + singletonBean2);
         assertThat(singletonBean1).isSameAs(singletonBean2);
-
-        ac.close();
+        ac.close(); //종료
     }
 
     @Scope("singleton")
@@ -38,5 +35,4 @@ public class SingletonTest {
             System.out.println("SingletonBean.destroy");
         }
     }
-
 }
